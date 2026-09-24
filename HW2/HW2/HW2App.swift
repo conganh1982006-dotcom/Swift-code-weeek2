43b14cc58b0954ff2645a5187a173f8a6d04816e
MAY 02 <may02@NewMAC02.local>
2026-09-24 10:47:26 +0700

    Add HW2 project

---
+++ HW2/HW2/HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW2App.swift
+//  HW2
+//
+//  Created by MAY 02 on 24/9/26.
+//
+
+import SwiftUI
+
+@main
+struct HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HW2/HW2Tests/HW2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  HW2Tests.swift
+//  HW2Tests
+//
+//  Created by MAY 02 on 24/9/26.
+//
+
+import Testing
+
+struct HW2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
